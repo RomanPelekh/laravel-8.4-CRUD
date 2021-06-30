2e162454af554be2ce281b6f760cdd964c8c3fed
RomanPelekh <pelekh.r.ya@gmail.com>
2021-06-30 15:53:09 +0300

    store and update action

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+@section('title','Users')
+@section('content')
+    <a class="btn btn-primary" role="button" href="{{ route('users.create') }}" >Create user</a>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                <a type="button" class="btn btn-warning" href="{{ route('users.edit', $user) }}">Edit</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    @endsection
